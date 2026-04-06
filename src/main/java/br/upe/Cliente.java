c3b26860f1bb02bdd866fa3b5cba6b945a30ef22
EricFelix <ericfelixfelixpinto@gmail.com>
2026-04-05 21:10:26 -0300

    Corrigindo um erro da classe Cliente

--- src/main/java/br/upe/Cliente.java
+++ src/main/java/br/upe/Cliente.java
@@ -8,7 +8,7 @@ public class Cliente {
     public Cliente (String nome, String email, Endereco endereco){
         this.nome = nome;
         this.email = email;
-        this.endereco  endereco;
+        this.endereco  = endereco;
     }
 
     public String getNome() {
